e8684c4f5c077623a0a8a9d43ad99d26fa24d4ba
Breno Nathanael <breno_nathanael@hotmail.com>
2023-06-07 15:12:01 -0300

    update

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { router } from "./routes/router.tsx";
 import { RouterProvider } from "react-router-dom";
-import ReactQuery from "./utils/reactQuery.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ReactQuery>
-      <RouterProvider router={router} />
-    </ReactQuery>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
