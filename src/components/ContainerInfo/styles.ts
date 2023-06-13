7bdc763d815a995f271736311e499a39587d6327
Breno Nathanael <breno_nathanael@hotmail.com>
2023-06-13 15:46:54 -0300

    update layout

--- src/components/ContainerInfo/styles.ts
+++ src/components/ContainerInfo/styles.ts
@@ -1,168 +1,169 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const ContainerInfo = styled.div`
-	width: 45%;
+  width: 45%;
+  max-height: 88vh;
 
-	border: 1px solid rgba(0, 0, 0, 0.4);
-	border-radius: 8px;
+  border: 1px solid rgba(0, 0, 0, 0.4);
+  border-radius: 8px;
 
-	background-color: #fffafa;
+  background-color: #fffafa;
 
-	display: flex;
-	flex-direction: column;
+  display: flex;
+  flex-direction: column;
 
-	padding: 12px;
+  padding: 12px;
 
-	overflow-y: scroll;
+  overflow-y: scroll;
 
-	::-webkit-scrollbar {
-		width: 4px;
-	}
+  ::-webkit-scrollbar {
+    width: 4px;
+  }
 
-	::-webkit-scrollbar-thumb {
-		background-color: rgba(0, 0, 0, 0.1);
-	}
+  ::-webkit-scrollbar-thumb {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
 
-	@media screen and (max-width: 600px) {
-		display: none;
-	}
+  @media screen and (max-width: 600px) {
+    display: none;
+  }
 `;
 
 export const ContainerTitleVaga = styled.div`
-	display: flex;
-	flex-direction: column;
-	gap: 8px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
 
-	h2 {
-		font-size: 2rem;
-		font-weight: 800;
-	}
+  h2 {
+    font-size: 2rem;
+    font-weight: 800;
+  }
 
-	div {
-		display: flex;
-		justify-content: flex-start;
-		gap: 12px;
-		margin-bottom: 12px;
+  div {
+    display: flex;
+    justify-content: flex-start;
+    gap: 12px;
+    margin-bottom: 12px;
 
-		span {
-			background-color: #696969;
+    span {
+      background-color: #696969;
 
-			padding: 4px;
+      padding: 4px;
 
-			border-radius: 4px;
+      border-radius: 4px;
 
-			color: #fff;
-		}
-	}
+      color: #fff;
+    }
+  }
 
-	a {
-		width: 50%;
-		padding: 0.8rem 1.2rem;
+  a {
+    width: 50%;
+    padding: 0.8rem 1.2rem;
 
-		cursor: pointer;
+    cursor: pointer;
 
-		border: 1px solid rgba(0, 0, 0, 0.1);
-		border-radius: 8px;
-		background-color: #a9a9a9;
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    border-radius: 8px;
+    background-color: #a9a9a9;
 
-		font-size: 1rem;
-		font-weight: bold;
-		text-align: center;
+    font-size: 1rem;
+    font-weight: bold;
+    text-align: center;
 
-		transition: all 0.2s ease-in-out;
+    transition: all 0.2s ease-in-out;
 
-		:hover {
-			background-color: #696969;
-			color: #fff;
-		}
-	}
+    :hover {
+      background-color: #696969;
+      color: #fff;
+    }
+  }
 `;
 
 export const ContainerDados = styled.div`
-	display: flex;
-	flex-direction: column;
-	gap: 12px;
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
 `;
 
 export const Title = styled.h2`
-	font-size: 1.6rem;
+  font-size: 1.6rem;
 `;
 
 export const ContainerInfoDados = styled.div`
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
-	div {
-		h4 {
-			margin-bottom: 8px;
-		}
+  div {
+    h4 {
+      margin-bottom: 8px;
+    }
 
-		p {
-			margin-left: 8px;
-		}
-	}
+    p {
+      margin-left: 8px;
+    }
+  }
 `;
 
 export const ContainerDescription = styled.div`
-	display: flex;
-	flex-direction: column;
-	gap: 12px;
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
 
-	h3 {
-		font-size: 1.3rem;
-	}
+  h3 {
+    font-size: 1.3rem;
+  }
 
-	p {
-		font-size: 0.9rem;
-		margin-left: 1.5rem;
-		text-align: start;
-	}
+  p {
+    font-size: 0.9rem;
+    margin-left: 1.5rem;
+    text-align: start;
+  }
 
-	div {
-		display: flex;
-		flex-direction: column;
-		gap: 8px;
+  div {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
 
-		h5 {
-			font-size: 1rem;
-		}
+    h5 {
+      font-size: 1rem;
+    }
 
-		ul {
-			margin-left: 1.5rem;
-			font-size: 0.9rem;
-		}
-	}
+    ul {
+      margin-left: 1.5rem;
+      font-size: 0.9rem;
+    }
+  }
 `;
 
 export const ContainerInput = styled.div`
-	width: 50%;
-	display: flex;
-	gap: 8px;
-	justify-content: center;
+  width: 50%;
+  display: flex;
+  gap: 8px;
+  justify-content: center;
 `;
 
 export const Input = styled.input`
-	width: 50%;
+  width: 50%;
 
-	padding-left: 8px;
+  padding-left: 8px;
 
-	background-color: #fff;
+  background-color: #fff;
 `;
 
 export const Button = styled.button`
-	padding: 0.5rem 3rem;
+  padding: 0.5rem 3rem;
 
-	background-color: #b22222;
-	color: #fff;
+  background-color: #b22222;
+  color: #fff;
 
-	border: none;
+  border: none;
 
-	cursor: pointer;
+  cursor: pointer;
 
-	transition: all 0.4s ease-in-out;
+  transition: all 0.4s ease-in-out;
 
-	&:hover {
-		background-color: #ff0000;
-	}
+  &:hover {
+    background-color: #ff0000;
+  }
 `;
